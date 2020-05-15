892df7efafc9ec31510337910777d03c032e195f
lamlyg <lamlyg@naver.com>
2020-05-15 14:24:54 +0900

    함수 filestat2 구현

--- main.c
+++ main.c
@@ -4,13 +4,14 @@
 #include <unistd.h>
 #include <time.h>
 
-struct stat stat1;
+struct stat stat1, stat2;
 
 void filestat1();
-
+void filestat2();
 
 int main() {
 	filestat1();
+	filestat2();
 }
 
 void filestat1() {
@@ -20,3 +21,9 @@ void filestat1() {
 	printf("blocks : %d\n", (int)stat1.st_blocks);
 }
 
+void filestat2() {
+	printf("text2 information\n");
+	stat("text2", &stat2);
+	printf("size : %d\n", (int)stat2.st_size);
+	printf("blocks : %d\n", (int)stat2.st_blocks);
+}
